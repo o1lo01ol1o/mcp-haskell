cc42ef9498f1b7277559ef77124fa8972a9f3390
o1lo01ol1o <tim.pierson@gmail.com>
2025-10-07 09:54:56 +0100

    Redirect logging to file for MCP servers and update Codex config

--- src/Utils/Logging.hs
+++ src/Utils/Logging.hs
@@ -2,17 +2,53 @@
 
 module Utils.Logging where
 
+import Control.Exception (IOException, catch)
+import Data.Maybe (isJust)
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
-import System.IO (stderr)
+import System.Directory (createDirectoryIfMissing, getXdgDirectory)
+import System.Environment (lookupEnv)
+import System.FilePath ((</>), takeDirectory)
+import System.IO (BufferMode (LineBuffering), Handle, IOMode (AppendMode), hFlush, hSetBuffering, openFile, stderr)
+import System.IO.Unsafe (unsafePerformIO)
 
 -- Log Level
 data LogLevel = Debug | Info | Warn | Error deriving (Show, Eq, Ord)
 
+-- Internal logging target
+data LogTarget = LogTarget
+  { ltHandle :: Handle
+  , ltAlsoStderr :: Bool
+  }
+
+{-# NOINLINE logTarget #-}
+logTarget :: LogTarget
+logTarget = unsafePerformIO $ do
+  mPath <- lookupEnv "MCP_LOG_FILE"
+  let acquireDefault = do
+        base <- getXdgDirectory XdgState "mcp-hls"
+        pure (base </> "mcp.log")
+
+  path <- case mPath of
+    Just explicit -> pure explicit
+    Nothing -> acquireDefault `catch` \(_ :: IOException) -> pure "/tmp/mcp-hls.log"
+
+  catch (createDirectoryIfMissing True (takeDirectory path)) $ \(_ :: IOException) -> pure ()
+  handle <- catch (openFile path AppendMode) $ \(_ :: IOException) -> openFile "/tmp/mcp-hls.log" AppendMode
+  hSetBuffering handle LineBuffering
+  alsoStderr <- fmap isJust (lookupEnv "MCP_LOG_STDERR")
+  pure LogTarget { ltHandle = handle, ltAlsoStderr = alsoStderr }
+
 -- Log Message
 logMessage :: LogLevel -> Text -> IO ()
-logMessage level msg = TIO.hPutStrLn stderr $ "[" <> T.pack (show level) <> "] " <> msg
+logMessage level msg = do
+  let line = "[" <> T.pack (show level) <> "] " <> msg
+  TIO.hPutStrLn (ltHandle logTarget) line
+  hFlush (ltHandle logTarget)
+  if ltAlsoStderr logTarget
+    then TIO.hPutStrLn stderr line
+    else pure ()
 
 -- Convenience functions
 logDebug, logInfo, logWarn, logError :: Text -> IO ()
